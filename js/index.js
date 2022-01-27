d913642f094520f9ff5a8093f9ae3cc3e0692b14
ibraa9 <essaidi.ib@hotmail.com>
2022-01-27 03:09:40 +0100

    integration of wallet connexion responsive page

---
+++ js/index.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    var sliderTeam = document.querySelector(".team .slider")
+    var action = document.querySelector(".article7 .slider")
+    sliderTeam.scrollLeft=230
+    action.scrollLeft=90
+}
