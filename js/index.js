4ae4ebcf1d0b8e11a9c0edd7ee56c88660190689
Ibrahim <essaidi.ib@hotmail.com>
2022-02-02 17:42:12 +0100

    no message

--- js/index.js
+++ js/index.js
@@ -1,4 +1,45 @@
 window.onload = () => {
+    /**
+     * Text arround the yellow circle in article1
+     */
+    var text = document.querySelector(".circleAnimation .text")
+    var textAround = text.textContent.split("") 
+    textAround.push(" ")           
+    let result = ""; 
+    textAround.forEach(function(char){
+        result +=  "<span>" + char + "</span>";
+    });
+    text.innerHTML = result; 
+    var listRotate = []
+    y=0
+    for (var i=0; i<text.children.length; i++) {
+        listRotate.push(y)
+
+        text.children[i].style.transform=`rotate(${y.toString()}deg)`
+        y+=360/text.children.length
+    }
+
+    //Animation : 
+    rotate=0
+    setInterval(() => {
+        for (var i=0; i<text.children.length; i++) {
+            if(i==text.children.length-1) {
+                text.children[i].style.transform=`rotate(0deg)`
+                console.log(text.children[i])
+            }
+            else {
+                text.children[i].style.transform=`rotate(${listRotate[i+rotate]}deg)`
+            }
+        }
+        rotate+=1
+        if (rotate==text.children.length) {
+            rotate=0
+        }
+    },1000)
+    /*
+    
+    }*/
+
     var sliderTeam = document.querySelector(".team .slider")
     var action = document.querySelector(".article7 .slider")
     sliderTeam.scrollLeft=230
